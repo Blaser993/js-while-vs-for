189fb0e0b1882b1361b91631fe1fbda3a9240202
Blaser993 <corradodepinto.design@gmail.com>
2023-02-07 14:29:56 +0100

    START

---
+++ script/myscript.js
@@ -0,0 +1,8 @@
+console.log("ciao")
+
+// Descrizione:
+// Utilizziamo un ciclo per chiedere all’utente un numero tramite prompt. Se il numero è dispari, lo salviamo in una lista. Continuiamo a chiedere un numero all’utente finchè non avremo ottenuto 10 numeri dispari. Tutti gli altri valori li scartiamo (numeri pari ed eventuali NaN).
+// Una volta ottenuti tutti i 10 numeri, stampiamoli in console uno alla volta.
+// Consigli:
+// dove conviene utilizzare il ciclo while e dove il ciclo for?
+// come faccio a capire se un numero è pari o dispari?
