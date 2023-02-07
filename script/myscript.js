f1df4b96c2b8b1a8082a975b11396cc1ec376cc3
Blaser993 <corradodepinto.design@gmail.com>
2023-02-07 15:36:27 +0100

    FEAT aggiungo numeri alla lista fino al limite

--- script/myscript.js
+++ script/myscript.js
@@ -5,15 +5,13 @@ console.log("ciao");
 // Descrizione:
 // Utilizziamo un ciclo per chiedere all’utente un numero tramite prompt. Se il numero è dispari, lo salviamo in una lista. Continuiamo a chiedere un numero all’utente finchè non avremo ottenuto 10 numeri dispari. Tutti gli altri valori li scartiamo (numeri pari ed eventuali NaN).
 // Una volta ottenuti tutti i 10 numeri, stampiamoli in console uno alla volta.
-// Consigli:
-// dove conviene utilizzare il ciclo while e dove il ciclo for?
-// come faccio a capire se un numero è pari o dispari?
-
-// Utilizziamo un ciclo per chiedere all’utente un numero tramite prompt. 
 
 
-let numero = parseInt(prompt("scrivi un numero"));
-console.log(numero, typeof(numero));
+// Utilizziamo un ciclo per chiedere all’utente un numero tramite prompt.
+let numero
+
+// let numero = parseInt(prompt("scrivi un numero"));
+// console.log(numero, typeof(numero));//
 
 // Se il numero è dispari, lo salviamo in una lista.
 
@@ -22,11 +20,28 @@ const numeriDispari = [];
 const DISPARI = numero % 2 != 0
 
 
-console.log(numero, DISPARI);
+// console.log(numero, DISPARI);
 
-if (numero == DISPARI){
+//  if (numero == DISPARI){
 
-    numeriDispari.push(numero);
+//     numeriDispari.push(numero);
+//  }
+
+// console.log(numeriDispari);
+
+// Continuiamo a chiedere un numero all’utente finchè non avremo ottenuto 10 numeri dispari.
+
+const LIMITE = 10
+
+while(numeriDispari.length < LIMITE){
+    console.log(numeriDispari)
+    let numero = parseInt(prompt("scrivi un numero"))
+
+    if (numero == DISPARI && !isNaN.numero){
+        console.log(numero)
+
+        numeriDispari.push(numero);
+    }
 }
 
-console.log(numeriDispari);
+console.log(numeriDispari)
