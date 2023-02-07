6abc6f168b5ef5eeda6a0be62c04ed298fed99fe
Blaser993 <corradodepinto.design@gmail.com>
2023-02-07 14:47:19 +0100

    FEAT pusho i numeri dispari in una lista

--- script/myscript.js
+++ script/myscript.js
@@ -1,4 +1,6 @@
-console.log("ciao")
+console.log("ciao");
+
+
 
 // Descrizione:
 // Utilizziamo un ciclo per chiedere all’utente un numero tramite prompt. Se il numero è dispari, lo salviamo in una lista. Continuiamo a chiedere un numero all’utente finchè non avremo ottenuto 10 numeri dispari. Tutti gli altri valori li scartiamo (numeri pari ed eventuali NaN).
@@ -10,7 +12,21 @@ console.log("ciao")
 // Utilizziamo un ciclo per chiedere all’utente un numero tramite prompt. 
 
 
-let numero = parseInt(prompt("scrivi un numero")) 
-console.log(numero, typeof(numero))
+let numero = parseInt(prompt("scrivi un numero"));
+console.log(numero, typeof(numero));
 
 // Se il numero è dispari, lo salviamo in una lista.
+
+const numeriDispari = [];
+
+const DISPARI = numero % 2 != 0
+
+
+console.log(numero, DISPARI);
+
+if (numero == DISPARI){
+
+    numeriDispari.push(numero);
+}
+
+console.log(numeriDispari);
